b555abfbe00e79f5684c0a1ec09bbbcc36eb4a79
Ivan <Ivan@DESKTOP-4QS6SFC>
2025-05-16 19:39:57 +0300

    додав файл для запису

---
+++ Storage.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using Newtonsoft.Json;
+
+namespace CalorieCounterApp
+{
+    public static class Storage
+    {
+        private static string filePath = "products.json";
+
+        public static void Save(List<Product> products)
+        {
+            var json = JsonConvert.SerializeObject(products, Newtonsoft.Json.Formatting.Indented);
+            File.WriteAllText(filePath, json);
+        }
+
+        public static List<Product> Load()
+        {
+            if (!File.Exists(filePath))
+                return new List<Product>();
+
+            var json = File.ReadAllText(filePath);
+            return JsonConvert.DeserializeObject<List<Product>>(json);
+        }
+    }
+}
